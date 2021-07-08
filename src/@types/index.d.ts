f3bffae74b7ef54be7502b1af095661e39a6405b
Masayuki Onishi <omccello@gmail.com>
2021-07-08 12:18:59 +0900

    fix

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -3,6 +3,12 @@ module Tableau {
   const Viz = (renderElement: HTMLDivElement, url: string, options?: VizOptions) => any;
 }
 
+type VizOptions = {
+  hideTabs?: boolean;
+  devise?: 'desktop';
+  onFirstInteractive?: Function;
+};
+
 interface Window {
   tableau: Tableau;
 }

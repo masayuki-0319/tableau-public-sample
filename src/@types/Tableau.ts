218e70975ce48ad62a7e6f197ba9e3f3b36eb4c1
将之大西 <omccello@gmail.com>
2021-07-10 12:40:07 +0900

    fix

---
+++ src/@types/Tableau.ts
@@ -0,0 +1,14 @@
+export interface Tableau {
+  Viz: Viz;
+}
+
+interface Viz {
+  new (renderElement: HTMLDivElement, url: string, options?: VizOptions): any;
+}
+
+type VizOptions = {
+  hideTabs?: boolean;
+  devise?: 'desktop';
+  onFirstInteractive?: Function;
+  [key: string]: any;
+};
--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,17 +1,4 @@
-interface Tableau {
-  Viz: Viz;
-}
-
-interface Viz {
-  new (renderElement: HTMLDivElement, url: string, options?: VizOptions): any;
-}
-
-type VizOptions = {
-  hideTabs?: boolean;
-  devise?: 'desktop';
-  onFirstInteractive?: Function;
-  [key?: string]: any;
-};
+import { Tableau } from './Tableau';
 
 interface Window {
   tableau: Tableau;
